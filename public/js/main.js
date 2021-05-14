1b1805a4542d6307fefefb534e5f1dad0a1407f3
M16 <Adilmohamedmohamed97@gmail.com>
2021-05-14 10:22:04 +0100

    Exo5

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+console.log('___1___');
+
+let phrase='Bonjour tout le monde ';
+console.log(phrase.length);
+console.log(phrase.trim());
+console.log(phrase.charAt(20));
+console.log(phrase.replace('Bonjour',''));
+alert(phrase)
+console.log(phrase.substr(0,8));
+
